98c597a270e8f1a697237f27e5da852835c95d1d
AugustoM94 <email@github.com>
2023-10-11 15:10:08 +0200

    add ciclo on js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,27 @@
 
 
 const wrapperEl = document.getElementById('boxWrapper');
+let element;
+
+
+for (let i = 1; i <= 100; i++) {
+    let bgBox;
+	if (i % 3 == 0 && i % 5 == 0) {
+        console.log("FizzBuzz");
+	} else if (i % 5 == 0) {
+        console.log("Buzz");
+	} else if (i % 3 == 0) {
+		console.log("Fizz");
+	} else {
+	     element = (i);
+        console.log(i)
+
+
+ const boxEl = document.createElement('div');
+boxEl.className = `box ${bgBox} d-flex justify-content-center align-items-center m-2`;
+boxEl.innerHTML =  i;
+wrapperEl.append(boxEl);
+	}
+}
+
+
