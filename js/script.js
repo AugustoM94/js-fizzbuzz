05f75fde9b9650b6c794f412f2b16b88b0b7cefe
AugustoM94 <email@github.com>
2023-10-12 10:15:21 +0200

    Project completed

--- js/script.js
+++ js/script.js
@@ -1,27 +1,34 @@
 
 
-const wrapperEl = document.getElementById('boxWrapper');
-let element;
+const mybox = document.getElementById('my-box');
 
 
 for (let i = 1; i <= 100; i++) {
-    let bgBox;
-	if (i % 3 == 0 && i % 5 == 0) {
-        console.log("FizzBuzz");
-	} else if (i % 5 == 0) {
-        console.log("Buzz");
-	} else if (i % 3 == 0) {
-		console.log("Fizz");
-	} else {
-	     element = (i);
-        console.log(i)
 
+        const div = document.createElement("div")
+        div.classList.add('my-small-box')
+        let text, bg;
 
- const boxEl = document.createElement('div');
-boxEl.className = `box ${bgBox} d-flex justify-content-center align-items-center m-2`;
-boxEl.innerHTML =  i;
-wrapperEl.append(boxEl);
-	}
+        if (i % 3 == 0 && i % 5 == 0) {
+                console.log('FizzBuzz')
+                bg = 'bgfizzbuzz';
+                text = `FizzBuzz`;
+        } else if (i % 5 == 0) {
+                console.log('Buzz')
+                bg = 'bgbuzz';
+                text = `Buzz`;
+        } else if (i % 3 == 0) {
+                console.log('Fizz')
+                bg = 'bgfizz';
+                text = `Fizz`;
+        } else {
+                console.log('i')
+                bg = 'bgdefault';
+                text = (i);
+        }
+  div.classList.add(bg);
+  div.innerText = text;
+  mybox.append(div);
+
 }
-
 
